8d4bb0d7049ae550e1da4fd6c90a8ae5b34a2218
marcela <marceladefranchi59@yahoo.es>
2023-03-11 18:15:41 -0300

    se agrega nodemon y algunas configuraciones

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+// Middlewares
+
+//Permite compartir recursos
+app.use(cors());
+
+//Contenido de carpeta public declarado como estatico
+app.use(express.static(__dirname + '/public'));
+
+//Configuracion de css, que accedera directamente a carpeta de bootstrap descargado en node_modules
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+//configuracion handlebars
+app.set('view engine', 'handlebars');
+app.engine(
+    'handlebars',
+    exphbs.engine({
+        defaultLayout: 'main',
+        layoutsDir: `${__dirname}/views/layouts`,
+        partialsDir: `${__dirname}/views/partials`,
+        extname: ".handlebars"
+    })
+);
 
 //puerto
 const puerto = process.env.PORT || 4000;
