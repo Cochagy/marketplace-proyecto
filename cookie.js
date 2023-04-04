9ba39a5e0df31781f89813bbc733d2ca60f50a88
Jorge Molina <95000105+Cochagy@users.noreply.github.com>
2023-04-03 20:36:11 -0400

    funciona enviar solicitud

--- cookie.js
+++ cookie.js
@@ -2,6 +2,10 @@ const cookie = (req, res, next) => {
     if (!req.cookies.retoken) {
         return res.redirect('/403')
     }
+
+    // Almacenar la información de la cookie 'retoken' en req.userCookie
+    req.userCookie = req.cookies.retoken;
+
     next();
 }
 
